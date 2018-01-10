f7635ed086e7b52f20a5f02b061764028eacdcc1
Daniela <daniela.barrientos.grisales@gmail.com>
2018-01-10 18:30:07 -0500

    Adding translations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {UIRouterModule} from "@uirouter/angular";
+import {HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from '../app/dashboard/dashboard.component';
 import { ProfilesComponent } from '../app/Profiles/profiles.component';
 import { FooterComponent } from '../app/shared/footer/footer.component';
 import { NavBarComponent } from '../app/shared/navbar/nav-bar.component';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 /** States */
-
 let dashboardState = { name: 'dashboard', url: '/dashboard',  component: DashboardComponent };
 let profilesState = { name: 'profiles', url: '/profiles',  component: ProfilesComponent };
 
@@ -17,10 +24,18 @@ let profilesState = { name: 'profiles', url: '/profiles',  component: ProfilesCo
     AppComponent, DashboardComponent, ProfilesComponent, FooterComponent, NavBarComponent
   ],
   imports: [
-    BrowserModule,
-    UIRouterModule.forRoot({ states: [ dashboardState, profilesState ], useHash: true })
+    BrowserModule, HttpClientModule,
+    UIRouterModule.forRoot({ states: [ dashboardState, profilesState ], useHash: true }),
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
-  providers: [],
+  providers: [ ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/shared/navbar/nav-bar.component.ts
+++ src/app/shared/navbar/nav-bar.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
-    selector: 'wavi-nav-bar',
-    templateUrl: './nav-bar.component.html',
-    styleUrls: ['./nav-bar.component.scss']
+  selector: 'wavi-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.scss']
 })
+
 export class NavBarComponent implements OnInit {
+  constructor(private translate: TranslateService) {
+    translate.setDefaultLang('en');
+  }
 
-    constructor() { }
+  switchLanguage(language: string) {
+    this.translate.use(language);
+  }
 
-    ngOnInit() {
-    }
+  ngOnInit() {}
 
 }
