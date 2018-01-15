28bf45548661200f90e0ce42281c4c60c00555b5
Daniela <daniela.barrientos.grisales@gmail.com>
2018-01-15 04:32:51 -0500

    Moving code from root component to VideosService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {UIRouterModule} from "@uirouter/angular";
 import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from '../app/dashboard/dashboard.component';
 import { ProfilesComponent } from '../app/Profiles/profiles.component';
 import { FooterComponent } from '../app/shared/footer/footer.component';
 import { NavBarComponent } from '../app/shared/navbar/nav-bar.component';
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+import { VideosService } from '../app/core/categories/videos.service';
+
 
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http);
@@ -33,27 +36,9 @@ let profilesState = { name: 'profiles', url: '/profiles',  component: ProfilesCo
       }
     })
   ],
-  providers: [ ],
+  providers: [ VideosService ],
   bootstrap: [AppComponent]
 })
 export class AppModule{
-
-
-
-  // function searchVideos(){
-  //
-  //   let vimeoConfig = {
-  //     'API_HOST': 'https://api.vimeo.com/',
-  //     'ACCESS_TOKEN': 'effa86d0284056eacb391c194926a789',
-  //     'PER_PAGE' : 12,
-  //     'SORT_DIRECTION' : 'asc',
-  //     'MAX_PAGINATOR_SIZE' : 3,
-  //     'FIRST_PART_URL' : 'https://i.vimeocdn.com/'
-  //   };
-  //
-  //   this.httpClient.get({
-  //     url: vimeoConfig.API_HOST + 'categories/' + 'sports' + '/videos?per_page=12',
-  //     headers: {Authorization: 'Bearer ' + vimeoConfig.ACCESS_TOKEN}
-  //   });
-  // }
+  
 }
---
+++ src/app/core/categories/videos.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class VideosService {
+
+    constructor(private http: HttpClient) {
+    }
+
+    getVideosByCategory() : void {
+
+    this.http.get<any>('https://api.vimeo.com/'+ 'categories/' + 'sports' + '/videos?per_page=12', {
+    headers:
+        new HttpHeaders()
+        .append('Authorization', 'Bearer d2f633a193b8d0be379f4d72cf0256f3')
+        }).subscribe(
+            data => {
+                console.log('data: ', data);
+            },
+            err => {
+                console.log("Error occured.")
+            }
+        );
+    }
+
+    
+}
